b24af1f78322dcc89640adecaf1f32cc72ee4c22
Nathan Chu <63111210+nthnchu@users.noreply.github.com>
2020-07-30 19:12:36 -0400

    Update themes.js

--- themes.js
+++ themes.js
@@ -1,3 +1,4 @@
+#!/usr/bin/env node
 const fs = require('fs');
 const themes = [ "default", "default_repocard", "dark", "radical", "merko", "gruvbox", "tokyonight", "onedark", "cobalt", "synthwave", "highcontrast", "dracula", "prussian", "monokai", "vue", "shades-of-purple", "nightowl", "buefy", "blue-green", "algolia", "great-gatsby" ];
 const theme = themes[Math.floor(Math.random() * themes.length)];
